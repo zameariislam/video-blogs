6333c5fab9870477582b48c80ab6b385dc46355e
zameariislam <zameariislam@gmail.com>
2022-10-04 13:04:38 +0200

    search feature added

--- src/app/store.js
+++ src/app/store.js
@@ -3,12 +3,14 @@ import videosReducer from '../features/videos/videosSlice';
 import videoReducer from '../features/video/videoSlice';
 import tagsReducer from '../features/tags/tagsSlice'
 import relatedVideosReducer from '../features/relatedVideos/relatedVideosSlice'
+import filterReducer from '../features/filter/filterSlice'
 
 export const store = configureStore({
   reducer: {
     videos: videosReducer,
     tags: tagsReducer,
-    video:videoReducer,
+    video: videoReducer,
     relatedVideos: relatedVideosReducer,
+    filter: filterReducer
   },
 });
--- src/components/tags/Tag.js
+++ src/components/tags/Tag.js
@@ -1,18 +1,54 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { tagRemoved, tagSelected } from '../../features/filter/filterSlice';
 
-const Tag = ({tag}) => {
+const Tag = ({ tag }) => {
+
+    const { tags, search } = useSelector(state => state.filter)
+    const dispatch = useDispatch()
+
+
+    const selected = tags.includes(tag.title)
+
+    const style=selected? "bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer"
+    :"bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer"
+    
+
+
+
+    const handleSelect = () => {
+
+        if (selected) {
+            dispatch(tagRemoved(tag.title))
+
+
+        }
+
+        else {
+            dispatch(tagSelected(tag.title))
+
+        }
+
+
+
+
+
+    }
+
+
+
     return (
-        <div
-            class="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer"
+        <div onClick={handleSelect}
+            class={style}
         >
-          {tag.title}
+            {tag.title}
         </div>
 
         // <div
-        //     class="bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer"
-        // >
-        //     redux
-        // </div>
+        // //     class="bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer"
+        // // >
+        // //     redux
+        // // </div>
     );
 };
 
